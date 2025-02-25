26d8900a00f0763f499f0185e7f34396f64984f1
Gaini Laxman <gainilaxman12@gmail.com>
2025-02-25 15:54:25 +0530

    first commit

---
+++ src/FBC.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function FBC() {
+  return <div></div>;
+}
+
+//========================================================
+
+//========================================================
+
+//rfc ==> for function based component
+//rcc ==> for class based component
+//rce ==> for class based component with export default
+//rcep ==> for class based component with export default and props types
+//rpc ==> for class based component with props types
+//rconst ==> for constructor with props
